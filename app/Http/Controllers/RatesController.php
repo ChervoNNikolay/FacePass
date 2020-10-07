e1bed7e764862d0ebc1e04a91e4f3203c9306e18
ChervoNNikolay <chervon.nikolai@gmail.com>
2020-10-07 18:23:42 +0500

    create method "getApi()"

--- app/Http/Controllers/RatesController.php
+++ app/Http/Controllers/RatesController.php
@@ -6,5 +6,13 @@ use Illuminate\Http\Request;
 
 class RatesController extends Controller
 {
-    //
+    protected $api;
+
+
+    private function getApi()
+    {
+        $api = file_get_contents('https://www.cbr-xml-daily.ru/daily_json.js');
+        $this->api = json_decode($api);
+    }
+
 }
