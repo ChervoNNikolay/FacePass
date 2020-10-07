7fb73060b9ef838c319134e98a674fa06eef31a1
ChervoNNikolay <chervon.nikolai@gmail.com>
2020-10-07 18:02:10 +0500

    create Routs

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::get('/exchange-rates', 'RatesController@showLastRate');
+
+Route::get('/exchange-rates/history/', 'RatesController@showRatesHistory');
